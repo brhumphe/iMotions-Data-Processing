c75f8ee3a8f100d60da9132217ce17fa73c5e738
Benjamin Humpherys <mooglinux@gmail.com>
2018-06-08 11:46:48 -0700

    Added per-second averages

---
+++ sql/avg_second.sql
@@ -0,0 +1,16 @@
+SELECT
+  Name,
+  Age,
+  Gender,
+--   Timestamp,
+  Timestamp / 1000 AS "Timestamp",
+  avg(Classification)  AS "Classification",
+  avg(HighEngagement)  AS "HighEngagement",
+  avg(LowEngagement)   AS "LowEngagement",
+  avg(Distraction)     AS "Distraction",
+  avg(Drowsy)          AS "Drowsy",
+  avg(WorkloadFBDS)    AS "WorkloadFBDS",
+  avg(WorkloadBDS)     AS "WorkloadBDS",
+  avg(WorkloadAverage) AS "WorkloadAverage"
+FROM eeg
+GROUP BY (Timestamp / 1000)
