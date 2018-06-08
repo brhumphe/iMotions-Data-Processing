27cf073846746e1a551bffea90e4b577f50f5b7e
Benjamin Humpherys <mooglinux@gmail.com>
2018-06-08 11:42:22 -0700

    Added average by postmarker query

---
+++ sql/average_postmarkers.sql
@@ -0,0 +1,18 @@
+SELECT
+  Name,
+  Age,
+  Gender,
+  PostMarker,
+  avg(Classification)  AS "Classification",
+  avg(HighEngagement)  AS "HighEngagement",
+  avg(LowEngagement)   AS "LowEngagement",
+  avg(Distraction)     AS "Distraction",
+  avg(Drowsy)          AS "Drowsy",
+  avg(WorkloadFBDS)    AS "WorkloadFBDS",
+  avg(WorkloadBDS)     AS "WorkloadBDS",
+  avg(WorkloadAverage) AS "WorkloadAverage"
+FROM eeg
+WHERE
+  PostMarker NOT NULL
+GROUP BY Name, Age, Gender, PostMarker
+ORDER BY Name
