b0b09c108d64c4d29f034a2d4702fc8b179d64f5
Benjamin Humpherys <mooglinux@gmail.com>
2018-10-11 15:52:54 -0700

    Removed failed zip handing code, tested multiprocessing code.

--- pandas_filter.py
+++ pandas_filter.py
@@ -4,10 +4,9 @@ import pandas as pd
 import os.path
 import glob
 from utils import open_file, filter_events, read_events, read_cols
-from zipfile import ZipFile
 
 
-def process_file(filename, event_sources, add_types=None, chunksize=50000):
+def process_file(filename, event_sources, add_types=None, chunksize=500000):
     data = open_file(filename, event_sources, add_types=add_types, chunksize=chunksize)
     frames = []
     for chunk in data:
@@ -15,22 +14,32 @@ def process_file(filename, event_sources, add_types=None, chunksize=50000):
     return pd.concat(frames)
 
 
-out_dir = "out/ecomm/"
+def process_to_csv(path, out_dir, event_sources):
+    print("Cleaning ", path)
+    base_name = os.path.basename(path)
+    try:
+        df = process_file(path, event_sources, chunksize=50000)
 
-archive = ZipFile("data/Sensor Data.zip")
-# files = [info.filename for info in archive.infolist()]
-events = ['ABMBrainState']
+        out_path = out_dir + os.path.splitext(base_name)[0] + '.csv'
+        df[df['SlideType'] == 'TestImage'].to_csv(out_path, index=False)
+    except ValueError as e:
+        print("Count not process ", path, file=sys.stderr)
+        print(e, file=sys.stderr)
+        print(read_events(path))
+        print(read_cols(path))
 
-for file in glob.glob("data/Sensor Data/*.txt"):
-        print("Cleaning ", file)
-        base_name = os.path.basename(file)
-        try:
-            df = process_file(file, events)
 
-            out_path = out_dir + os.path.splitext(base_name)[0] + '.csv'
-            df[df['SlideType'] == 'TestImage'].to_csv(out_path, index=False)
-        except ValueError as e:
-            print("Count not process ", file, file=sys.stderr)
-            print(e, file=sys.stderr)
-            print(read_events(file))
-            print(read_cols(file))
+if __name__ == '__main__':
+    from multiprocessing import Pool
+    from functools import partial
+
+    destination_dir = "out/r111/"
+    events = ['Performance Metrics Epoc']
+
+    files = glob.glob("data/001_r111/*.txt")
+    func = partial(process_to_csv, out_dir=destination_dir, event_sources=events)
+
+    pool = Pool(1)  # Leave at 1 if IO bound to reduce read contention on disk
+    pool.map(func, files)
+    pool.close()
+    pool.join()
